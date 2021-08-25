d0aac7e20da212d0e928e21aed6ffe98f75dc757
Jean Souza <jeanhrsouza@hotmail.com>
2021-08-25 08:33:40 -0300

    Serviço de autenticação

---
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AutenticacaoService {
+  //injeção de dependência
+  constructor(private httpClient: HttpClient) {}
+
+  //Observable é como se fosse uma promise do JS
+  //O observable retornará um Objeto assim que for completada a requisição que
+  autenticar(usuario: string, senha: string): Observable<any> {
+    return this.httpClient.post('http://localhost:3000/user/login', {
+      //mapeando os objetos
+      userName: usuario,
+      password: senha,
+    });
+  }
+}
