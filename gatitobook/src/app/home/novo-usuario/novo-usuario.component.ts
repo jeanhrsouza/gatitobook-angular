2d6ec43f3b77e9e9cf8c8b3a49184faf481a37c1
Jean Souza <jeanhrsouza@hotmail.com>
2021-09-27 21:10:41 -0300

    Servico de novo usuario e formulario reativo

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { NovoUsuario } from './novo-usuario';
+import { NovoUsuarioService } from './novo-usuario.service';
 
 @Component({
   selector: 'app-novo-usuario',
   templateUrl: './novo-usuario.component.html',
-  styleUrls: ['./novo-usuario.component.css']
+  styleUrls: ['./novo-usuario.component.css'],
 })
 export class NovoUsuarioComponent implements OnInit {
+  // a ! indica que pode ser nulo ou não
+  novoUsuarioForm!: FormGroup;
 
-  constructor() { }
+  //injetando o servico
+  constructor(
+    private formBuilder: FormBuilder,
+    private novoUsuarioService: NovoUsuarioService
+  ) {}
 
+  //O ngOnInit é executado após a classe injetar todo o servico;
   ngOnInit(): void {
+    this.novoUsuarioForm = this.formBuilder.group({
+      email: [''],
+      fullName: [''],
+      userName: [''],
+      password: [''],
+    });
   }
 
+  //executado quando for submetido
+  cadastrar() {
+    /*
+      Utilizando o getRawValue
+      É retornado um objeto somente com o estado das variáveis carregadas
+      da view que o usuário carregou
+    */
+    const novoUsuario = this.novoUsuarioForm.getRawValue() as NovoUsuario;
+    console.log('Cadastrou');
+  }
 }
