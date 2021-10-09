a9cad73ae70d86ff4654ff830c5875566786789a
Jean Souza <jeanhrsouza@hotmail.com>
2021-10-09 20:39:19 -0300

    feat(autenticacao): add servico de autenticacao

--- gatitobook/src/app/autenticacao/autenticacao.service.ts
+++ gatitobook/src/app/autenticacao/autenticacao.service.ts
@@ -1,6 +1,8 @@
+import { UsuarioService } from './usuario/usuario.service';
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
 
 // @Injectable indica que a classe pode ser injetada em algum outro serviço
 // Transforma o objeto em um Singleton.
@@ -9,15 +11,35 @@ import { Observable } from 'rxjs';
 })
 export class AutenticacaoService {
   //injeção de dependência
-  constructor(private httpClient: HttpClient) {}
+  constructor(
+    private httpClient: HttpClient,
+    private usuarioService: UsuarioService
+  ) {}
 
   //Observable é como se fosse uma promise do JS
   //O observable retornará um Objeto assim que for completada a requisição que
-  autenticar(usuario: string, senha: string): Observable<any> {
-    return this.httpClient.post('http://localhost:3000/user/login', {
-      //mapeando os objetos
-      userName: usuario,
-      password: senha,
-    });
+  /*
+    Toda vez que fizer uma requisição, além de pegar as requisições
+    eu quero salvar o token no meu serviço utilizando o tap
+  */
+  autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this.httpClient
+      .post(
+        'http://localhost:3000/user/login',
+        {
+          //mapeando os objetos
+          userName: usuario,
+          password: senha,
+        },
+        {
+          observe: 'response',
+        }
+      )
+      .pipe(
+        tap((res) => {
+          const authToken = res.headers.get('x-access-token') ?? '';
+          this.usuarioService.salvaToken(authToken);
+        })
+      );
   }
 }
