acfc18c47ac59a6ac19764172078c2bf9a79506f
Jean Souza <jeanhrsouza@hotmail.com>
2021-10-09 16:46:19 -0300

    feat(novo-usuario): validacao  usuario e senha iguais

--- gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
+++ gatitobook/src/app/home/novo-usuario/novo-usuario.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { minusculoValidator } from './minusculo.validator';
 import { NovoUsuario } from './novo-usuario';
 import { NovoUsuarioService } from './novo-usuario.service';
+import { usuarioSenhaIguaisValidator } from './usuario-senhas-iguais.validator';
 
 @Component({
   selector: 'app-novo-usuario',
@@ -26,6 +27,9 @@ export class NovoUsuarioComponent implements OnInit {
       fullName: ['', [Validators.required, Validators.minLength(4)]],
       userName: ['', [minusculoValidator]],
       password: [''],
+    },
+    {
+      validators: [usuarioSenhaIguaisValidator],
     });
   }
 
