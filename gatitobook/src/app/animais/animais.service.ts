3b9b939a7e726ca21b53797ce694646b1b65b607
Jean Souza <jeanhrsouza@hotmail.com>
2021-12-31 17:32:18 -0300

    feat(controle-de-fluxo-de-navegacao): metodos de curtir e excluir foto

--- gatitobook/src/app/animais/animais.service.ts
+++ gatitobook/src/app/animais/animais.service.ts
@@ -1,11 +1,13 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { catchError, mapTo } from 'rxjs/operators';
 import { environment } from './../../environments/environment';
 import { TokenService } from './../autenticacao/token.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { Animais, Animal } from './animais';
 
 const API = environment.apiURL;
+const NOT_MODIFIED = '304';
 
 @Injectable({
   providedIn: 'root',
@@ -14,12 +16,29 @@ export class AnimaisService {
   constructor(private http: HttpClient, private tokenService: TokenService) {}
 
   listaDoUsuario(nomeDoUsuario: string): Observable<Animais> {
-
     //Realizando chama da API passando um header
     return this.http.get<Animais>(`${API}/${nomeDoUsuario}/photos`);
   }
 
   buscaPorID(id: number): Observable<Animal> {
     return this.http.get<Animal>(`${API}/photos/${id}`);
+  }
+
+  excluiAnimal(id: number): Observable<Animal> {
+    return this.http.delete<Animal>(`${API}/photos/${id}`);
+  }
+
+  curtir(id: number): Observable<boolean> {
+    // O 3ª parametro é para receber o status
+    return this.http
+      .post(`${API}/photos/${id}/likes`, {}, { observe: 'response' })
+      .pipe(
+        //mapTo: retornará true para o componente
+        mapTo(true),
+        catchError((error) => {
+          //of: observable que retorna valor
+          return error.status === NOT_MODIFIED ? of(false) : throwError(error);
+        })
+      );
   }
 }
